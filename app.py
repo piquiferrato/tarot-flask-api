3b47cea61f0a287ad04c6bd6c0f0c5700d106634
piquiferrato <juanferrato1994@gmail.com>
2020-01-05 01:50:02 -0300

    test without jsonify (flask 1.1 doesnt need it)

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import random
 
-from flask import Flask, jsonify
+from flask import Flask
 from deck import cards
 
 app = Flask(__name__)
@@ -10,30 +10,30 @@ def ping():
   endpoints = [rule.rule for rule in app.url_map.iter_rules() 
   if rule.endpoint !='static']
   
-  return jsonify(dict(api_endpoints=endpoints))
+  return (dict(api_endpoints=endpoints))
 
 @app.route('/tarot/deck/', methods=['GET'])
 def getDeck():
-  return jsonify({"cards": cards})
+  return ({"cards": cards})
 
 @app.route('/tarot/deck/card/<int:card_number>/', methods=['GET'])
 def getCard(card_number):
   for card in cards:
     if card['id'] == card_number:
-      return jsonify({"card": card})
+      return ({"card": card})
   
-  return jsonify({'message': 'Card does not exist in this deck'})
+  return ({'message': 'Card does not exist in this deck'})
 
 
 @app.route('/tarot/deck/draft/', methods=['GET'])
 def dealCards():
   draft = random.sample(cards, 3)
-  return jsonify({'draft': draft})
+  return ({'draft': draft})
 
 @app.route('/tarot/deck/card/random/', methods=['GET'])
 def getRandomCard():
   card = random.choice(cards)
-  return jsonify({'card': card})
+  return ({'card': card})
 
 if __name__ == '__main__':
   app.run(debug=True, use_reloader=True)
